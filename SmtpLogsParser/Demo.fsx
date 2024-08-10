7d78783118ccdc213fcb417b3606d359ff15625c
AlexKalnitskiy <kalnitskiy@mindbox.cloud>
2024-08-10 20:48:50 +0300

    *

--- SmtpLogsParser/Demo.fsx
+++ SmtpLogsParser/Demo.fsx
@@ -12,9 +12,9 @@ let test p str =
     | Success(result, _, _)   -> printfn "Success: %A" result
     | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
           
-let multipleEmailsParser =
-    sepBy emailParser spaces1 .>> eof |>> String.concat " -+- "
+let OverlordParser =
+    sepBy (choice [ attempt emailParser; ipParser ]) spaces1 .>> eof |>> String.concat " -+- "
     
 test ipParser "192.43.142.84";
 test emailParser "surrenderplease@gmail.com!#@";
-test multipleEmailsParser "surrend.erplease@gmail.com sur333333333renderplease@gmail.com surrenderplease@gmail.com"
+test OverlordParser "surrend.erplease@gmail.com sur333333333renderplease@gmail.com 192.43.142.84 surrenderplease@gmail.com"
